9d0c490b86e0f8a539b190c40de9e7b121ec9687
LillLEE <kopqwe147@gmail.com>
2021-09-27 00:08:37 +0900

    Calculator Requires Additional Exceptions

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,86 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Calculator {
+
+    private String[] values;
+
+    private Queue<String> numberQueue = new LinkedList<String>();
+    private Queue<String> nonNumberQueue = new LinkedList<String>();
+
+    private final String[] nonNumber = {"+", "-", "*", "/"};
+
+    public Calculator() {
+    }
+
+    public Calculator(Queue<String> numberQueue, Queue<String> nonNumberQueue) {
+        this.numberQueue = numberQueue;
+        this.nonNumberQueue = nonNumberQueue;
+    }
+
+    private void ErrorCheck1(Queue<String> numberQueue, Queue<String> nonNumberQueue) {
+        if (nonNumberQueue.size() >= numberQueue.size()) {
+            throw new IllegalStateException("Wrong Value.");
+        }
+    }
+
+    private void ErrorCheck(String[] values) {
+        if ((values.length < 3)) {
+            throw new IllegalStateException("Wrong Value.");
+        }
+        for (String s : nonNumber) {
+            if ((values[0].contains(s)) || (values[values.length - 1].contains(s))) {
+                throw new IllegalStateException("Wrong Value.");
+            }
+        }
+    }
+
+    public void StringEmptyCheck(String value) {
+        if (value.isEmpty()) {
+            throw new IllegalStateException("Empty Value.");
+        }
+    }
+
+    private Integer FindTypeAndCalculator(Queue<String> numberQueue,
+                                          Queue<String> nonNumberQueue) {
+        Integer currentNumber;
+        Integer resultNumber;
+        String operator;
+
+        resultNumber = Integer.parseInt(numberQueue.remove());
+        while (!(numberQueue.isEmpty())) {
+            currentNumber = Integer.parseInt(numberQueue.remove());
+            operator = nonNumberQueue.remove();
+            switch (operator) {
+                case "+":
+                    resultNumber += currentNumber;
+                    break;
+                case "-":
+                    resultNumber -= currentNumber;
+                    break;
+                case "*":
+                    resultNumber *= currentNumber;
+                    break;
+                case "/":
+                    resultNumber /= currentNumber;
+                    break;
+            }
+        }
+        return resultNumber;
+    }
+
+    protected Integer InputData(final String value) {
+        StringEmptyCheck(value);
+        values = value.split(" ");
+        ErrorCheck(values);
+        for (int i = 0; i < values.length; i++) {
+            if (i % 2 == 0) {
+                numberQueue.add(values[i]);
+            } else {
+                nonNumberQueue.add(values[i]);
+            }
+        }
+        ErrorCheck1(numberQueue, nonNumberQueue);
+        return FindTypeAndCalculator(numberQueue, nonNumberQueue);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,65 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void stringSplitTest() {
+        String str1;
+        String str2;
+        String[] splitNumber1;
+        String[] splitNumber2;
+
+        str1 = "1,2";
+        str2 = "1";
+        splitNumber1 = str1.split(",");
+        splitNumber2 = str2.split(",");
+
+        assertThat(splitNumber1).contains("1", "2");
+        assertThat(splitNumber2).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("SubString Value Return Test")
+    void subStringTest() {
+        String str1;
+        String str2;
+        String subString;
+        int charAt_Index;
+
+        str1 = "(1,2)";
+        str2 = "abc";
+        subString = str1.substring(1, 4);
+        charAt_Index = str2.charAt(2);
+
+        assertThat(subString).contains("1,2");
+    }
+
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+
 }
